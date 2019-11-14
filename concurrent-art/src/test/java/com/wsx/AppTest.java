908394904b372e1947221d92c3225e17c3a1db62
wusx <wusx@sutpc.com>
2019-11-14 17:41:19 +0800

    1、数据结构与算法模块 2、并发编程艺术

---
+++ concurrent-art/src/test/java/com/wsx/AppTest.java
@@ -0,0 +1,20 @@
+package com.wsx;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
