d9f5f218835f8f3f5174354729e5de907cc097d4
wusx <wusx@sutpc.com>
2020-04-16 16:59:19 +0800

    1、thinking in netty

---
+++ thinking-in-netty/src/main/java/com/wusx/thinkinginnetty/ThinkingInNettyApplication.java
@@ -0,0 +1,13 @@
+package com.wusx.thinkinginnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingInNettyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThinkingInNettyApplication.class, args);
+  }
+
+}
---
+++ thinking-in-netty/src/test/java/com/wusx/thinkinginnetty/ThinkingInNettyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wusx.thinkinginnetty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThinkingInNettyApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
