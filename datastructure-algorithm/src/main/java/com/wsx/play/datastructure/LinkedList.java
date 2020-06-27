dd534cbcf932696ecc1cbb775894c671a44fec83
ap0908330 <283235702@qq.com>
2020-06-27 23:46:00 +0800

    1、单链表

--- datastructure-algorithm/src/main/java/com/wsx/play/datastructure/LinkedList.java
+++ datastructure-algorithm/src/main/java/com/wsx/play/datastructure/LinkedList.java
@@ -8,6 +8,140 @@ package com.wsx.play.datastructure;
  */
 public class LinkedList<E> {
 
+  //private Node Head;
+  //使用虚拟头结点
+  private Node dummyHead;
+  private int size;
+
+  public LinkedList() {
+    dummyHead = new Node(null, null);
+    size = 0;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  public void addFirst(E e) {
+    //Node node = new Node(e);
+    //node.next = head.next;
+    //head.next = node;
+    //head = new Node(e, head);
+    add(0, e);
+    //size++;
+  }
+
+  public void addLast(E e) {
+    add(size, e);
+  }
+
+  public void add(int index, E e) {
+    if (index < 0 || index > size) {
+      throw new IllegalArgumentException("Add failed. Illegal index.");
+    }
+    //if (index == 0) {
+    //  addFirst(e);
+    //} else {
+    //  Node pre = head;
+    Node pre = dummyHead;
+    for (int i = 0; i < index - 1; i++) {
+      pre = pre.next;
+    }
+    //Node node = new Node(e);
+    //node.next = pre.next;
+    //pre.next = node;
+    pre.next = new Node(e, pre.next);
+    size++;
+    //}
+  }
+
+  public E getFirst() {
+    return get(0);
+  }
+
+
+  public E getLast() {
+    return get(size - 1);
+  }
+
+
+  public E get(int index) {
+    if (index < 0 || index > size) {
+      throw new IllegalArgumentException("Get failed. Illegal index.");
+    }
+    Node cur = dummyHead.next;
+    for (int i = 0; i < index; i++) {
+      cur = cur.next;
+    }
+    return cur.e;
+  }
+
+  public void set(int index, E e) {
+    if (index < 0 || index > size) {
+      throw new IllegalArgumentException("Set failed. Illegal index.");
+    }
+    Node cur = dummyHead.next;
+    for (int i = 0; i < index; i++) {
+      cur = cur.next;
+    }
+    cur.e = e;
+  }
+
+  public boolean contains(E e) {
+    Node cur = dummyHead.next;
+    while (cur.next != null) {
+      if (cur.e.equals(e)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public E remove(int index) {
+    if (index < 0 || index > size) {
+      throw new IllegalArgumentException("Remove failed. Illegal index.");
+    }
+    Node pre = dummyHead;
+    for (int i = 0; i < index; i++) {
+      pre = pre.next;
+    }
+    Node target = pre.next;
+    pre.next = pre.next.next;
+    target.next = null;
+    size--;
+    return target.e;
+  }
+
+  public E removeFirst() {
+    return remove(0);
+  }
+
+  public E removeLast() {
+    return remove(size - 1);
+  }
+
+
+  @Override
+  public String toString() {
+
+    StringBuilder builder = new StringBuilder();
+
+    Node cur = dummyHead.next;
+    builder.append("lindedList [");
+    while (null != cur.next) {
+      builder.append(cur.e);
+      cur = cur.next;
+      builder.append("->");
+    }
+    builder.append("null");
+
+    return builder.toString();
+  }
+
   private class Node {
 
     public E e;
@@ -30,6 +164,22 @@ public class LinkedList<E> {
     public String toString() {
       return e.toString();
     }
+  }
+
+  public static void main(String[] args) {
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    for (int i = 0; i < 10; i++) {
+      linkedList.addFirst(i);
+    }
+    System.out.println(linkedList);
+    linkedList.addLast(10);
+    System.out.println(linkedList);
+    linkedList.set(2, 8);
+    System.out.println(linkedList);
+    linkedList.removeFirst();
+    System.out.println(linkedList);
+    linkedList.removeLast();
+    System.out.println(linkedList);
   }
 
 }
