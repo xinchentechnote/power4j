c456048cfe82444be8f12555012186eb53a50f34
AP0908330 <283235702@qq.com>
2020-07-05 22:54:21 +0800

    1、测试map

--- datastructure-algorithm/src/main/java/com/wsx/play/datastructure/map/BSTMap.java
+++ datastructure-algorithm/src/main/java/com/wsx/play/datastructure/map/BSTMap.java
@@ -1,6 +1,5 @@
 package com.wsx.play.datastructure.map;
 
-import lombok.NoArgsConstructor;
 
 /**
  * @Description .
@@ -32,7 +31,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
 
   @Override
   public void add(K key, V value) {
-    add(root, key, value);
+    root = add(root, key, value);
   }
 
   private Node add(Node node, K key, V value) {
@@ -41,9 +40,9 @@ public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
       return new Node(key, value);
     } else {
       if (key.compareTo(node.key) > 0) {
-        return add(node.right, key, value);
+        node.right = add(node.right, key, value);
       } else if (key.compareTo(node.key) < 0) {
-        return add(node.left, key, value);
+        node.left = add(node.left, key, value);
       } else {
         node.value = value;
       }
@@ -51,33 +50,118 @@ public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
     return node;
   }
 
+  private Node getNode(Node node, K key) {
+    if (null == node) {
+      return null;
+    }
+    if (key.compareTo(node.key) == 0) {
+      return node.right;
+    } else if (key.compareTo(node.key) > 0) {
+      return getNode(node.right, key);
+    } else {
+      return getNode(node.left, key);
+    }
+  }
+
   @Override
   public V remove(K key) {
-    return null;
+    Node node = getNode(root, key);
+    if (null == node) {
+      return null;
+    }
+    root = removeNode(root, key);
+    return node.value;
+  }
+
+  private Node removeNode(Node node, K key) {
+    if (key.compareTo(node.key) < 0) {
+      return removeNode(node.left, key);
+    } else if (key.compareTo(node.key) > 0) {
+      return removeNode(node.right, key);
+    } else {
+      if (null == node.left) {
+        Node right = node.right;
+        node.right = null;
+        size--;
+        return right;
+      }
+      if (null == node.right) {
+        Node left = node.left;
+        node.left = null;
+        size--;
+        return left;
+      }
+
+      Node minimum = minimum(node.right);
+      minimum.right = removeMin(node.right);
+      minimum.left = node.left;
+      node.left = node.right = null;
+      return minimum;
+    }
+  }
+
+  private Node minimum(Node node) {
+    if (null != node.left) {
+      return minimum(node.left);
+    }
+    return node;
+  }
+
+  private Node removeMin(Node node) {
+    if (null == node.left) {
+      Node right = node.right;
+      node.right = null;
+      size--;
+      return right;
+    }
+    node.left = removeMin(node.left);
+    return node;
   }
 
   @Override
   public boolean contains(K key) {
-    return false;
+    return getNode(root, key) == null;
   }
 
   @Override
   public V get(K key) {
-    return null;
+    Node node = getNode(root, key);
+    return null == node ? null : node.value;
   }
 
   @Override
   public void set(K key, V value) {
-
+    Node node = getNode(root, key);
+    if (null != node) {
+      node.value = value;
+    } else {
+      throw new IllegalArgumentException("key dosn't exist.");
+    }
   }
 
   @Override
   public int getSize() {
-    return 0;
+    return this.size;
   }
 
   @Override
   public boolean isEmpty() {
-    return false;
+    return this.size == 0;
   }
+
+  public static void main(String[] args) {
+
+    BSTMap<String, Integer> map = new BSTMap<>();
+    map.add("hello", 1);
+    map.add("hello", 1);
+    map.add("ap", 1);
+    map.add("test", 1);
+    map.add("world", 1);
+    System.out.println(map);
+    System.out.println(map.getSize());
+    map.remove("test");
+    System.out.println(map.getSize());
+    System.out.println(map);
+  }
+
 }
---
+++ datastructure-algorithm/src/main/java/com/wsx/play/datastructure/map/MapTest.java
@@ -0,0 +1,26 @@
+package com.wsx.play.datastructure.map;
+
+import com.google.common.base.Stopwatch;
+
+/**
+ * @Description .
+ * @Author:ShangxiuWu
+ * @Date: 22:49 2020/7/5.
+ * @Modified By:
+ */
+public class MapTest {
+
+  public static void main(String[] args) {
+    test(new BSTMap<>());
+    test(new LinkedListMap<>());
+  }
+
+  private static void test(Map<String, Integer> map) {
+    Stopwatch stopwatch = Stopwatch.createStarted();
+    for (int i = 0; i < 100_000; i++) {
+      map.add("key" + i, i);
+    }
+    System.out.println(stopwatch.stop().toString());
+  }
+
+}
