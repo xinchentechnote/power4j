ad8a80994ac3cb8beefa526d38c05b7aac5d3874
AP0908330 <283235702@qq.com>
2020-07-06 00:13:23 +0800

    1、最大堆

--- datastructure-algorithm/src/main/java/com/wsx/play/datastructure/MaxHeap.java
+++ datastructure-algorithm/src/main/java/com/wsx/play/datastructure/MaxHeap.java
@@ -7,6 +7,40 @@ package com.wsx.play.datastructure;
  * @Date: 9:23 2020/6/19.
  * @Modified By:
  */
-public class MaxHeap {
+public class MaxHeap<E extends Comparable<E>> {
+
+  private Array<E> data;
+
+  public MaxHeap() {
+    this.data = new Array<>();
+  }
+
+  public MaxHeap(int capacity) {
+    this.data = new Array<>(capacity);
+  }
+
+  private int parentIndex(int index) {
+    if (index == 0) {
+      throw new IllegalArgumentException("index 0 doesn't have parent.");
+    }
+    return (index - 1) / 2;
+  }
+
+  private int leftChildIndex(int index) {
+    return index * 2 + 1;
+  }
+
+  private int rightChildIndex(int index) {
+    return index * 2 + 2;
+  }
+
+
+  public int size() {
+    return data.size();
+  }
+
+  public boolean isEmpty() {
+    return data.isEmpty();
+  }
 
 }
