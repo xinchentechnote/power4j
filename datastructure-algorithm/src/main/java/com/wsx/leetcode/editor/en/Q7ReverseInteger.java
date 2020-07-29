96a5e96fba92a5ea12c1a7be969298b384c652f4
wusx <wusx@sutpc.com>
2020-07-29 18:30:19 +0800

    1、LeetCode 7 反转一个整数

---
+++ datastructure-algorithm/src/main/java/com/wsx/leetcode/editor/en/Q7ReverseInteger.java
@@ -0,0 +1,81 @@
+package com.wsx.leetcode.editor.en;
+
+//Given a 32-bit signed integer, reverse digits of an integer. 
+//
+// Example 1: 
+//
+// 
+//Input: 123
+//Output: 321
+// 
+//
+// Example 2: 
+//
+// 
+//Input: -123
+//Output: -321
+// 
+//
+// Example 3: 
+//
+// 
+//Input: 120
+//Output: 21
+// 
+//
+// Note: 
+//Assume we are dealing with an environment which could only store integers with
+//in the 32-bit signed integer range: [−231, 231 − 1]. For the purpose of this pro
+//blem, assume that your function returns 0 when the reversed integer overflows. 
+// Related Topics Math 
+// 👍 3467 👎 5475
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+//Java：Reverse Integer
+public class Q7ReverseInteger {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    // TO TEST
+    System.out.println(solution.reverse(-321));
+  }
+
+  static
+      //leetcode submit region begin(Prohibit modification and deletion)
+  class Solution {
+
+    public int reverse(int x) {
+      boolean symbol = (x & 0x80_00_00_00) == 0x80_00_00_00;
+      int usignX = Math.abs(x);
+      int i = 0;
+      Queue<Integer> queue = new LinkedList<>();
+      while ((usignX / pow(10, i)) != 0) {
+        int mode = (usignX % pow(10, i + 1) / pow(10, i));
+        queue.add(mode);
+        i++;
+      }
+      int result = 0;
+      for (int j = queue.size() - 1; j >= 0; j--) {
+        Integer remove = queue.remove();
+        result += remove * pow(10, j);
+      }
+      return symbol ? 0 - result : result;
+    }
+
+    private int pow(int a, int b) {
+      if (b == 0) {
+        return 1;
+      }
+      for (int i = 1; i < b; i++) {
+        a *= a;
+      }
+      return a;
+    }
+  }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
